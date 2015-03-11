6e76888409c045f313d71d951aae94e3efbc4f10
goncalomb <me@goncalomb.com>
2015-03-11 00:06:55 +0000

    Config file comments

--- www/_andi4http/config.php
+++ www/_andi4http/config.php
@@ -1,14 +1,19 @@
 <?php
 
+// Date format ('http://php.net/manual/en/function.date.php).
 $config['date-format'] = 'd-m-Y H:i:s';
 
+// Enable/Disable the JSON API (/?json=true).
 $config['json-api'] = true;
 
+// Show/Hide main table columns.
 $config['show-mimetype'] = false;
 $config['show-mtime'] = true;
 $config['show-mtime-dir'] = true;
 $config['show-size'] = true;
 
+// All entries set here will not be listed, they may still be accessible.
+// An .htaccess file prevents '_andi4http' from being accessible.
 $config['exclude-entries'] = array(
 	'_andi4http',
 	'index.php',
