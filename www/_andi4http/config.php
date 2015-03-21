d3ad783d21eeb1d1d18ac0f7c328b6c5c913e83e
goncalomb <me@goncalomb.com>
2015-03-21 19:37:53 +0000

    Comments fix

--- www/_andi4http/config.php
+++ www/_andi4http/config.php
@@ -5,6 +5,7 @@
     default   : The default hardcoded theme (also used as fallback).
     plain     : Simple theme without any styling. You can easily customize it
                 using the header.php file (see that file for more details).
+    example   : Example theme with comments to help you creating new themes.
     bootstrap : Bootstrap based theme.
 */
 $config['theme'] = 'bootstrap';
@@ -14,7 +15,7 @@ $config['theme-config']['bootstrap'] = array(
 	// Bootswatch theme, possible values:
 	// amelia, cerulean, cosmo, cyborg, darkly, flatly, journal, lumen, paper,
 	// readable, sandstone, simplex, slate, spacelab, superhero, united, yeti
-	// and none (to use the default Bootstrap style)
+	// and none (to use the default Bootstrap style).
 	'bootswatch' => 'readable'
 );
 
@@ -26,11 +27,11 @@ $config['date-format'] = 'd-m-Y H:i:s';
 // Enable/Disable the JSON API (/?json=true).
 $config['json-api'] = true;
 
-// Breadcrumbs
+// Show/Hide breadcrumbs.
 $config['breadcrumbs'] = true;
 $config['breadcrumbs-on-root'] = false;
 
-// Show parent link (../)
+// Show/Hide parent link (../).
 $config['parent-link'] = false;
 
 // Show/Hide main table columns.
